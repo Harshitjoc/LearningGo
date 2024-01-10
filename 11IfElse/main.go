ffb39e7096264c184414c43f6eb06de86dee5b30
Harshit Joshi <Harshitrocky9@gmail.com>
2024-01-10 11:52:10 +0530

    if else

---
+++ 11IfElse/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("If-Else in Golang")
+
+	loginCount := 23
+	var result string
+
+	if loginCount < 10 {
+		result = "Regular User"
+	} else if loginCount > 10 {
+		result = "Watch Out"
+	} else {
+		result = "Something Else"
+	}
+	fmt.Println(result)
+
+	if 9%2 == 0 {
+		fmt.Println("Number is even")
+	} else {
+		fmt.Println("Number is Odd")
+	}
+
+	if num := 3; num < 10 {
+		fmt.Println("Number is less than 10")
+	} else {
+		fmt.Println("Number is not less than 10")
+	}
+}
